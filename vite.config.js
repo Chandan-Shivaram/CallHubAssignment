e0f9e3903fb92bb847ac442387bda65fd177c7e7
Chandan S <chandan.shivaram12@gmail.com>
2023-08-25 13:18:37 +0530

    dist 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  proxy: {
-    target: 'http://localhost:8000/',
-  }
+  base: '/CallHubAssignment'
 })
